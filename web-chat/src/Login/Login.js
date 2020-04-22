c9f07c29eb7dd1d674fb342acd20870b0e33e0af
Itthithee <poomlong@gmail.com>
2020-04-23 04:55:34 +0700

    change search box and message send box to new design

--- web-chat/src/Login/Login.js
+++ web-chat/src/Login/Login.js
@@ -8,6 +8,14 @@ class Login extends React.Component{
         }
     }
     
+    fetch(){
+
+    }
+
+    componentDidMount(){
+
+    }
+
     render(){
         return(
             <div class="bgcolor">
@@ -21,15 +29,15 @@ class Login extends React.Component{
                                     </div>
                                 </div>
                                 <div class="row">
-                                    username
+                                    <label htmlFor="userId-input">username</label>
                                 </div>
                                 <div class="row">
                                     <div class="col-md-12">
-                                        <input class="inbox"/>
+                                        <input id="userId-input" class="inbox"/>
                                     </div>
                                 </div>
                                 <div class="row">
-                                    password
+                                    <label htmlFor="userId-input">password</label>
                                 </div>
                                 <div class="row">
                                     <div class="col-md-12">
